620d08154191ee81e8f2141600da2718361fe051
Rehan41302 <ggrowthmates.com@gmail.com>
2020-05-02 20:13:58 +0500

    Tada...Kid-Just-Made-A-Table-Generator

---
+++ Table.py
@@ -0,0 +1,7 @@
+
+
+
+print("Which Table do you want..??")
+operand=input()
+for i in range(10):
+    print(f"{operand} X {i+1} = {int(operand)*(i+1)}")
---
+++ app.py
@@ -0,0 +1,5 @@
+name=input()
+if name=="Rehan":
+    print(f"hello, {name}!")
+else:
+    print("Who are You Bitch..??")    
